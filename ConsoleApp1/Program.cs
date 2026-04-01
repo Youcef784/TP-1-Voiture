45b4d6fc3f2fb2ecdd329cc529780e295ac60fdf
Youcef784 <yousteck93@gmail.com>
2026-04-01 11:08:05 +0200

    TP5-3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,5 +4,5 @@ Reservoir r1 = new Reservoir(50);
 r1.diminuer(20);
 Console.WriteLine(r1.ToString());
 
-voiture v1 = new voiture("1234AB56", 150, r1);
+VoiturePersonnel v1 = new VoiturePersonnel("1234AB56", 150, r1);
 Console.WriteLine(v1.ToString());
